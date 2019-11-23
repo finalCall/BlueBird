17b7f9359dbdf3893b90a73ab6fcb4b93de07c09
iamNamanKhare <namankhare42@gmail.com>
2019-11-23 11:01:02 +0530

    add computation.js

--- contracts/Bird.sol
+++ contracts/Bird.sol
@@ -1,49 +1,55 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract bird{
-    int24 infi = int24(-1);
+    int _ = int(-1);
     uint orderID = 0;
-    mapping(uint => address payable) droneAddress;
+    address payable[] droneAddress = [0x086C9A35245cC1198440236665c8134e9FeD3D26,
+    0xa736FeD8e0C2F6317898E7090e57bBA2bB7c7D4b,
+    0x237799f9BE53976381eE1b2c2D5006DD23E3576b,
+    0x66B327C0Bd326c0ad748AeF5CF1CCB67F1012e29,
+    0x4E231D5BC3f18136f30120d98A78b8A22a2D2993,
+    0x7E65A2094a0048f48762C7e8E3C2d1348948aEf7
+    ];
+
+    // function to take input address for each drone
     /*
-    droneAddress[0] = ;
-    droneAddress[1] = ;
-    droneAddress[2] = ;
-    droneAddress[3] = ;
-    droneAddress[4] = ;
-    droneAddress[5] = ;
+    function addDroneAddr(address payable _address, uint index) public returns(string memory){
+        droneAddress[index] = _address;
+        return "OK droneAddr";
+    }
     */
+    //mnemonics : board weasel tip physical alien charge exhaust exclude approve cabin frost excite
     mapping(uint => uint[6]) paymentDue; // mapping from orderID to payment_due_to_each_drone
     //uint[] paymentOrderIDs;
 
     // array to be updated later
-    int24[6][6] distanceArray = [[infi,int24(1702),int24(2560),infi,int24(2560),int24(1702)],
-        [int24(1702),infi,int24(1381),int24(2730),infi,int24(3161)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)]
+    int[6][6] distanceArray = [[_,int(1702),int(2560),_,int(2560),int(1702)],
+        [int(1702),_,int(1381),int(2730),_,int(3161)],
+        [int(2560),int(1381),_,int(1792),int(3162),_],
+        [_,int(2730),int(1792),_,int(1792),int(2730)],
+        [int(2560),_,int(3162),int(1792),_,int(1382)],
+        [int(1702),int(3161),_,int(2730),int(1381),_]
     ];
 
-    int24[6][6] costArray = [[infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)],
-        [infi,int24(1),int24(1),int24(1),int24(1),int24(1)]
+    int[6][6] costArray = [[_, int(500), int(1000), _, int(1500), int(900)],
+        [int(500), _, int(400), int(1500), _, int(300)],
+        [int(1000), int(400), _, int(1000), int(400), _],
+        [_, int(1500), int(1000), _, int(500), int(1500)],
+        [int(1500), _, int(400), int(500), _, int(500)],
+        [int(900), int(300), _, int(1500), int(500), _]
     ];
 
     // function to fetch the updated Distance Array
-    function getMatrixDistance() public view returns(int24[6][6] memory){
+    function getMatrixDistance() public view returns(int[6][6] memory){
         return distanceArray;
     }
 
     // function to fetch the updated Cost Array
-    function getMatrixCost() public view returns(int24[6][6] memory){
+    function getMatrixCost() public view returns(int[6][6] memory){
         return costArray;
     }
 
     // function to update the Cost matrix and return an generated orderID
-    uint cost = 1; // dummy cost for adding to payment_due_to_each_drone
 
     function updateCostMatrix(uint[2][] memory pairs) public payable returns(uint){
         orderID++;
@@ -57,44 +63,22 @@ contract bird{
         for(uint i = 0; i<arrLen; i++){
             uint x = uint(pairs[i][0]);
             uint y = uint(pairs[i][1]);
-            costArray[x][y] = int24(costArray[x][y]) + 300; // increasing price by 20%
-            costArray[y][x] = int24(costArray[y][x]) + 300; // increasing price by 20%
-            //Let's assume 'cost' is being added to due paymeny array for this transaction
-            tempPay[x] = uint(costArray[x][y]) - 300;
+            tempPay[x] = uint(costArray[x][y]);
+            costArray[x][y] = int(costArray[x][y]) + 300; // increasing price by 300
+            costArray[y][x] = int(costArray[y][x]) + 300; // increasing price by 300
         }
         paymentDue[orderID] = tempPay;
         //paymentOrderIDs.push(orderID);
         return orderID;
     }
-    /*
-    function findIndex(uint[] memory arr, uint ele) public pure returns(uint){
-        uint arrLen = arr.length;
-        for(uint i = 0; i<arrLen; i++){
-            if(arr[i] == ele){
-                return i;
-            }
-        }
-    }
-    // Function to delete an orderID from paymentOrderIDs array;
-    function deleteElement(uint _orderID) public{
-        uint index = findIndex(paymentOrderIDs, _orderID);
-        if (index >= paymentOrderIDs.length) return;
-
-        for (uint i = index; i<paymentOrderIDs.length-1; i++){
-            paymentOrderIDs[i] = paymentOrderIDs[i+1];
-        }
-        delete paymentOrderIDs[paymentOrderIDs.length-1];
-        paymentOrderIDs.length--;
-    }
-    */
     function delieveryComplete(uint _orderID, uint[2][] memory pairs) public returns(string memory){
         //deleteElement(_orderID);
         uint arrLen = pairs.length;
         for(uint i = 0; i<arrLen; i++){
             uint x = uint(pairs[i][0]);
             uint y = uint(pairs[i][1]);
-            costArray[x][y] = int24(costArray[x][y]) - 300; // increasing price by 20%
-            costArray[y][x] = int24(costArray[y][x]) - 300; // increasing price by 20%
+            costArray[x][y] = int(costArray[x][y]) - 300; // decreasing price by 300
+            costArray[y][x] = int(costArray[y][x]) - 300; // decreasing price by 300
         }
         uint[6] memory temp = paymentDue[_orderID];
         for(uint i = 0; i<6; i++){
@@ -102,6 +86,6 @@ contract bird{
                 droneAddress[i].transfer(temp[i]);
             }
         }
-        return "Ho gaya BC !!!";
+        return "200_OK";
     }
 }
---
+++ public/computecost.js
@@ -0,0 +1,166 @@
+var dist12 = getDistance([wareHouseLat[0], wareHouseLong[0]], [wareHouseLat[1], wareHouseLong[1]]);
+var dist13 = getDistance([wareHouseLat[0], wareHouseLong[0]], [wareHouseLat[2], wareHouseLong[2]]);
+var dist15 = getDistance([wareHouseLat[0], wareHouseLong[0]], [wareHouseLat[4], wareHouseLong[4]]);
+var dist16 = getDistance([wareHouseLat[0], wareHouseLong[0]], [wareHouseLat[5], wareHouseLong[5]]);
+var dist23 = getDistance([wareHouseLat[1], wareHouseLong[1]], [wareHouseLat[2], wareHouseLong[2]]);
+var dist24 = getDistance([wareHouseLat[1], wareHouseLong[1]], [wareHouseLat[3], wareHouseLong[3]]);
+var dist26 = getDistance([wareHouseLat[1], wareHouseLong[1]], [wareHouseLat[5], wareHouseLong[5]]);
+var dist34 = getDistance([wareHouseLat[2], wareHouseLong[2]], [wareHouseLat[3], wareHouseLong[3]]);
+var dist35 = getDistance([wareHouseLat[2], wareHouseLong[2]], [wareHouseLat[4], wareHouseLong[4]]);
+var dist45 = getDistance([wareHouseLat[3], wareHouseLong[3]], [wareHouseLat[4], wareHouseLong[4]]);
+var dist46 = getDistance([wareHouseLat[3], wareHouseLong[3]], [wareHouseLat[5], wareHouseLong[5]]);
+var dist56 = getDistance([wareHouseLat[4], wareHouseLong[4]], [wareHouseLat[5], wareHouseLong[5]]);
+var srccntr = 0;
+var destcntr = 0;
+
+var _ = Infinity;
+
+var srccntr = 0;
+var destcntr = 0;
+var path1, path2, netcost1, netcost2;
+var cost;
+var distance;
+function computecost() {
+    
+    Coursetro.getMatrixCost((err, res)=>{
+        if(res){
+            console.log(res);
+            var dummy = res
+            for(var i=0; i<6; i++){
+                for(var j=0; j<6; j++){
+                    cost[i][j] = dummy[i][j].c;
+                }
+            }
+            console.log(cost);
+        }
+        else
+        console.log(err);
+    });/*
+    Coursetro.getMatrixDistance((err, res)=>{
+        if(res){
+            console.log(res);
+        }
+    });*/
+    if (flag1 == 1 && flag2 == 1) {
+        var nearestcentres = nearcntr();
+        srccntr = nearestcentres[0];
+        destcntr = nearestcentres[1];
+
+        // Compute the shortest path on the basis of cost
+        var shortestPathInfo1 = shortestPath(cost, 6, srccntr);
+
+        // Get the shortest path from vertex srccntr to vertex destcntr.
+        path1 = constructPath(shortestPathInfo1, destcntr);
+        path1.unshift(srccntr);
+
+        // Compute the shortest path on the basis of distance
+        var shortestPathInfo2 = shortestPath(distance, 6, srccntr);
+
+        // Get the shortest path from vertex srccntr to vertex destcntr.
+        path2 = constructPath(shortestPathInfo2, destcntr);
+        path2.unshift(srccntr);
+
+        netcost1 = 0;
+        for (var i = 0; i < path1.length - 1; i++) {
+            netcost1 += cost[path1[i]][path1[i + 1]];
+        }
+        //console.log(netcost1)
+        document.getElementById("p1").innerHTML = "Price: " + netcost1;
+
+        netcost2 = 0;
+        for (var i = 0; i < path2.length - 1; i++) {
+            netcost2 += cost[path2[i]][path2[i + 1]];
+        }
+
+        document.getElementById("p2").innerHTML = "Price: " + netcost2;
+        //console.log(netcost2)
+    }
+}
+map.on('click', computecost);
+
+var intervalId;
+var runcount ;
+var myMovingMarker = {};
+
+function PrintCostPath() {
+    if (flag1 == 1 && flag2 == 1) {
+        runcount=0;
+        function PrintPath() {
+            if (runcount == (path1.length - 1)) {
+                clearInterval(intervalId)
+            }
+            if (runcount != (path1.length - 1)) {
+                if (myMovingMarker != undefined) {
+                    map.removeLayer(myMovingMarker);
+                }
+                myMovingMarker = L.Marker.movingMarker([[wareHouseLat[path1[runcount]], wareHouseLong[path1[runcount]]], [wareHouseLat[path1[runcount + 1]], wareHouseLong[path1[runcount + 1]]]],
+                    [4000], { icon: droneIcon }).addTo(map).bindPopup("<b>Package is On The Way!</b><br>").openPopup();
+
+                myMovingMarker.start();
+            }
+            else {
+                if (myMovingMarker != undefined) {
+                    map.removeLayer(myMovingMarker);
+                }
+                myMovingMarker = L.Marker.movingMarker([[wareHouseLat[path1[(path1.length - 1)]], wareHouseLong[path1[(path1.length - 1)]]], [destinationLat, destinationLong]],
+                    [4000], { icon: packagedlvr }).addTo(map).bindPopup("<b>Package is Out For Delivery!</b><br>").openPopup();
+                myMovingMarker.start();
+                setTimeout(function(){ myMovingMarker.bindPopup("<b>Package Delivered!</b><br>").openPopup();}, 4000);
+
+            }
+            runcount++;
+        }
+
+
+        myMovingMarker = L.Marker.movingMarker([[sourceLat, sourceLong], [wareHouseLat[path1[0]], wareHouseLong[path1[0]]]],
+            [4000], { icon: packageacpt }).addTo(map);
+        myMovingMarker.start();
+
+        intervalId = setInterval(PrintPath, 4000)
+        curr = -1;
+        flag1 = 0, flag2 = 0;
+    }
+}
+
+function PrintDistPath() {
+    if (flag1 == 1 && flag2 == 1) {
+        runcount=0;
+        function PrintPath() {
+            if (runcount == (path2.length - 1)) {
+                clearInterval(intervalId)
+            }
+            if (runcount != (path2.length - 1)) {
+                if (myMovingMarker != undefined) {
+                    map.removeLayer(myMovingMarker);
+                }
+                myMovingMarker = L.Marker.movingMarker([[wareHouseLat[path2[runcount]], wareHouseLong[path2[runcount]]], [wareHouseLat[path2[runcount + 1]], wareHouseLong[path2[runcount + 1]]]],
+                    [4000], { icon: droneIcon }).addTo(map).bindPopup("<b>Package is On The Way!</b><br>").openPopup();
+
+                myMovingMarker.start();
+            }
+            else {
+                if (myMovingMarker != undefined) {
+                    map.removeLayer(myMovingMarker);
+                }
+                myMovingMarker = L.Marker.movingMarker([[wareHouseLat[path2[(path2.length - 1)]], wareHouseLong[path2[(path2.length - 1)]]], [destinationLat, destinationLong]],
+                    [4000], { icon: packagedlvr }).addTo(map).bindPopup("<b>Package is Out For Delivery!</b><br>").openPopup();
+                myMovingMarker.start();
+                setTimeout(function(){ myMovingMarker.bindPopup("<b>Package Delivered!</b><br>").openPopup();}, 4000);
+
+            }
+            runcount++;
+        }
+
+
+        myMovingMarker = L.Marker.movingMarker([[sourceLat, sourceLong], [wareHouseLat[path2[0]], wareHouseLong[path2[0]]]],
+            [4000], { icon: packageacpt }).addTo(map);
+        myMovingMarker.start();
+
+        intervalId = setInterval(PrintPath, 4000)
+        curr = -1;
+        flag1 = 0, flag2 = 0;
+    }
+}
+
+dist_deliver.addEventListener('click', PrintDistPath);
+cost_deliver.addEventListener('click', PrintCostPath);
