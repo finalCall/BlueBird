bcbede8a37af091f31cd305b3c079561d8ccad94
iamNamanKhare <namankhare42@gmail.com>
2019-11-24 13:30:45 +0530

    Modified contract ABI

--- public/connection.js
+++ public/connection.js
@@ -10,86 +10,86 @@ else {
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 var CoursetroContract = web3.eth.contract([
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getMatrixDistance",
-    "outputs": [
-      {
-        "internalType": "int256[6][6]",
-        "name": "",
-        "type": "int256[6][6]"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getMatrixCost",
-    "outputs": [
-      {
-        "internalType": "int256[6][6]",
-        "name": "",
-        "type": "int256[6][6]"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "internalType": "uint256[2][]",
-        "name": "pairs",
-        "type": "uint256[2][]"
-      }
-    ],
-    "name": "updateCostMatrix",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_orderID",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256[2][]",
-        "name": "pairs",
-        "type": "uint256[2][]"
-      }
-    ],
-    "name": "delieveryComplete",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-]);
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getMatrixDistance",
+      "outputs": [
+        {
+          "internalType": "int256[6][6]",
+          "name": "",
+          "type": "int256[6][6]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getMatrixCost",
+      "outputs": [
+        {
+          "internalType": "int256[6][6]",
+          "name": "",
+          "type": "int256[6][6]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256[2][]",
+          "name": "pairs",
+          "type": "uint256[2][]"
+        }
+      ],
+      "name": "updateCostMatrix",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_orderID",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256[2][]",
+          "name": "pairs",
+          "type": "uint256[2][]"
+        }
+      ],
+      "name": "deliveryComplete",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]);
 
-var Coursetro = CoursetroContract.at('0x41aC8966254731d496e72dF2D693Ea87C2f44A7d'); // to be updated later
+var Coursetro = CoursetroContract.at('0x05727aCE261c4DcD46A57C596186BedbcA2099bc'); // to be updated later
 
 /*
 for(var i=0; i<6; i++){
