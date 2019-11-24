e271a298bc7d91007f614fe79af26738a1451661
iamNamanKhare <namankhare42@gmail.com>
2019-11-24 20:53:04 +0530

    Modified ABI

--- public/connection.js
+++ public/connection.js
@@ -11,93 +11,125 @@ var accountBox = document.getElementById("accounts");
 accountBox.addEventListener('change', () => {
   web3.eth.defaultAccount = web3.eth.accounts[accountBox.value];
 })
-web3.eth.defaultAccount = web3.eth.accounts[0];
+web3.eth.defaultAccount = web3.eth.accounts[6];
 
 var CoursetroContract = web3.eth.contract([
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getMatrixDistance",
-      "outputs": [
-        {
-          "internalType": "int256[6][6]",
-          "name": "",
-          "type": "int256[6][6]"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getMatrixCost",
-      "outputs": [
-        {
-          "internalType": "int256[6][6]",
-          "name": "",
-          "type": "int256[6][6]"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "internalType": "uint256[2][]",
-          "name": "pairs",
-          "type": "uint256[2][]"
-        }
-      ],
-      "name": "updateCostMatrix",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_orderID",
-          "type": "uint256"
-        },
-        {
-          "internalType": "uint256[2][]",
-          "name": "pairs",
-          "type": "uint256[2][]"
-        }
-      ],
-      "name": "deliveryComplete",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ]);
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "addr",
+        "type": "address"
+      }
+    ],
+    "name": "orderPlaced",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "addr",
+        "type": "address"
+      }
+    ],
+    "name": "releaseAmount",
+    "type": "event"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getMatrixDistance",
+    "outputs": [
+      {
+        "internalType": "int256[6][6]",
+        "name": "",
+        "type": "int256[6][6]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getMatrixCost",
+    "outputs": [
+      {
+        "internalType": "int256[6][6]",
+        "name": "",
+        "type": "int256[6][6]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256[2][]",
+        "name": "pairs",
+        "type": "uint256[2][]"
+      }
+    ],
+    "name": "updateCostMatrix",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_orderID",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256[2][]",
+        "name": "pairs",
+        "type": "uint256[2][]"
+      }
+    ],
+    "name": "deliveryComplete",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getAccountDetails",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]);
 
 var Coursetro = CoursetroContract.at('0x05727aCE261c4DcD46A57C596186BedbcA2099bc'); // to be updated later
-
-/*
-for(var i=0; i<6; i++){
-    var send = web3.eth.accounts[i].toString();
-    var res = Coursetro.addDroneAddr(send,i);
-    console.log(res + " " + i);
-}*/
